3d6977f5f292b64ca2b17129598e01abe1aecbf2
maxbond <ceo@rf.ru>
2022-11-13 21:33:30 +0300

    f

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hello(props) {
+    return (
+      <div className="App">
+
+      <h1>Hello, {props.name}</h1>
+      
+      </div>
+    );
+  }
+  
+  export default Hello; 
